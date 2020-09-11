e30732518f50bbedaadaa127abd23a4108749a28
ABC <ABC@gmail.com>
2020-09-11 09:34:35 +0000

    .c

--- palindrome.c
+++ palindrome.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-int main() {
+void pal() {
     int n, reversedN = 0, remainder, originalN;
     printf("Enter an integer: ");
     scanf("%d", &n);
     originalN = n;
 
-    // reversed integer is stored in reversedN
     while (n != 0) {
         remainder = n % 10;
         reversedN = reversedN * 10 + remainder;
         n /= 10;
     }
 
-    // palindrome if orignalN and reversedN are equal
     if (originalN == reversedN)
-        printf("%d is a palindrome.", originalN);
+        printf("%d is a palindrome.\n", originalN);
     else
-        printf("%d is not a palindrome.", originalN);
+        printf("%d is not a palindrome.\n", originalN);
 
-    return 0;
+//    return 0;
 }
